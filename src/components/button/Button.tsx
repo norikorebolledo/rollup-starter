71f44018deec3287d5b9fc6034a91bf87db09417
Noriko <rebolledo.noriko@gmail.com>
2023-06-16 22:27:27 +0800

    Initial

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    label: string;
+}
+
+const Button = (props: Props) => {
+  return (
+    <button>{props.label}</button>
+  )
+}
+
+export default Button
